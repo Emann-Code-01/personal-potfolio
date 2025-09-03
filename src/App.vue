06f0fe47e8b7c29f1a7446f40c5e8e5098271f11
Emann-script <olajubajeifeoluwa93@gmail.com>
2025-09-03 14:37:18 +0100

    i removed the residue of the vercel/analytic left in my code

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@
 import Navbar from './components/Navbar.vue';
 import { RouterView } from 'vue-router';
 import Footer from './components/Footer.vue';
-import { Analytics } from "@vercel/analytics/next"
 </script>
 
 <style scoped></style>
